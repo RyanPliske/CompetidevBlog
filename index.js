25acf456d891063fa910d43dbbeae61005703527
Ryan Pliske <ryan.pliske@monsanto.com>
2016-01-16 14:57:06 -0600

    Added Carousel

--- index.js
+++ index.js
@@ -2,13 +2,9 @@
 
 function setup() {
   $( function() { 
-    // $("#navbar").load("navbar.html");
-    // $("#blog-header").load("blog-header.html");
-    // $("#sidebar").load("sidebar.html");
+    $("#navbar").load("navbar.html");
+    $("#myCarousel").load("carousel.html");
     // $("#post1").load("post1.html");
-    // $("#post2").load("post2.html");
-    // $("#post3").load("post3.html", highlight);
-    // $("#post4").load("post4.html", highlight);
     // $("#footer").load("footer.html");
   });
 }

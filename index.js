dd1ee5bbdfbf63797e1d1a6cd4c2424c26c783bc
Ryan Pliske <ryan.pliske@monsanto.com>
2016-01-16 18:11:54 -0600

    post1 setup to switchout html

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function setup() {
   $( function() { 
     $("#navbar").load("navbar.html");
     $("#myCarousel").load("carousel.html");
-    $("#post1-description").load("post1_description.html");
+    $("#post1-description").load("post1_description.html", function() {
+      $("#post1-description-link").click(requestPost1);
+    });
     $("#footer").load("footer.html");
   });
 }
@@ -13,4 +15,8 @@ var highlight = function() {
   $('pre code').each(function(i, block) {
     hljs.highlightBlock(block);
   });
+};
+
+var requestPost1 = function() {
+  $("#post1-description").load("post1.html");
 };

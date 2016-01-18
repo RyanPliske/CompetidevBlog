471d0589a019e1e35502e645c668bf3dc6675d96
Ryan Pliske <ryan.pliske@monsanto.com>
2016-01-18 12:37:03 -0600

    Added completion to requestingPost1

--- index.js
+++ index.js
@@ -15,23 +15,26 @@ var highlight = function() {
   });
 };
 
-var requestPost1 = function() {
+var requestPost1WithCompletion = function(completion) {
   $("#post1-description").load("post1.html", function() {
     $("#post1-link").click(requestPost1Description);
+    completion();
   });
 };
 
 var requestPost1Description = function() {
   $("#post1-description").load("post1_description.html", function() {
-    $("#post1-description-link").click(requestPost1);
+    $("#post1-description-link").click(requestPost1WithCompletion);
   });
 }
 
 var setupCarousel = function() {
   $("#myCarousel").load("carousel.html", function() {
     $("#carouselPost1Button").click( function() {
-      requestPost1();
-      document.getElementById("post1-description").scrollIntoView();
+      var completion = function() {
+        document.getElementById("post1-description").scrollIntoView();
+      }
+      requestPost1WithCompletion(completion);
     });
   });
 }

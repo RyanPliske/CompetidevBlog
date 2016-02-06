d5120b8560c61d48cb2da7af3580c5bd2bd6150a
Ryan Pliske <ryan.pliske@monsanto.com>
2016-02-06 16:36:54 -0600

    Cleanup

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 "use strict";
 
 var queryString = window.location.hash.substring(1);
-var numberOfPosts = 3;
 
-var setupCarousel = function() {
+function setupCarousel() {
   $("#myCarousel").load("carousel.html", function() {
     $("#carouselPost1Button").click( function() {
       var completion = function() {
@@ -20,52 +19,61 @@ var setupCarousel = function() {
   });
 };
 
-var setupDevs = function() {
-  $("#nicks-button").click(nicksButtonTapped);
-  $("#pats-button").click(patsButtonTapped);
-  $("#ryans-button").click(ryansButtonTapped);
-  $("#dans-button").click(dansButtonTapped);
+function DevManager() {
+  var dev1 = 'nicks';
+  var dev2 = "pats";
+  var dev3 = "ryans";
+  var dev4 = "dans";
+
+  this.ryansButtonTapped = function() {
+    handleTapForName(dev3);
+  };
+
+  this.dansButtonTapped = function() {
+    handleTapForName(dev4);
+  };
+
+  this.patsButtonTapped = function() {
+    handleTapForName(dev2);
+  };
+
+  this.nicksButtonTapped = function() {
+    handleTapForName(dev1);
+  };
+
+  var handleTapForName = function(developersName) {
+    var infoString = developersName + "-info";
+    var bioString = developersName + "-bio";
+    if ($("#" + infoString).hasClass("hidden")) {
+      $("#" + infoString).className = document.getElementById(infoString).classList.remove("hidden");
+      document.getElementById(bioString).classList.add("hidden");
+      $("#" + developersName + "-button").text("View Bio");
+    }
+    else if ($("#" + bioString).hasClass("hidden")) {
+      $("#" + bioString).className = document.getElementById(bioString).classList.remove("hidden");
+      document.getElementById(infoString).classList.add("hidden");
+      $("#" + developersName + "-button").text("Contact Info");
+    }
+  };
 };
 
-var handleTapForName = function(developersName) {
-  var infoString = developersName + "-info";
-  var bioString = developersName + "-bio";
-  if ($("#" + infoString).hasClass("hidden")) {
-    $("#" + infoString).className = document.getElementById(infoString).classList.remove("hidden");
-    document.getElementById(bioString).classList.add("hidden");
-    $("#" + developersName + "-button").text("View Bio");
-  }
-  else if ($("#" + bioString).hasClass("hidden")) {
-    $("#" + bioString).className = document.getElementById(bioString).classList.remove("hidden");
-    document.getElementById(infoString).classList.add("hidden");
-    $("#" + developersName + "-button").text("Contact Info");
-  }
-};
+function setupDevs() {
+  var devManager = new DevManager();
+  $("#nicks-button").click(devManager.nicksButtonTapped);
+  $("#pats-button").click(devManager.patsButtonTapped);
+  $("#ryans-button").click(devManager.ryansButtonTapped);
+  $("#dans-button").click(devManager.dansButtonTapped);
+}
 
-var ryansButtonTapped = function() {
-  handleTapForName("ryans");
-};
-
-var dansButtonTapped = function() {
-  handleTapForName("dans");
-};
-
-var patsButtonTapped = function() {
-  handleTapForName("pats");
-};
-
-var nicksButtonTapped = function() {
-  handleTapForName("nicks");
-};
-
-var highlight = function() {
+function highlight() {
   $('pre code').each(function(i, block) {
     hljs.highlightBlock(block);
   });
-};
+}
 
 var requestPostWithCompletion = function(postString, completion) {
   $("#" + postString).load(postString + ".html", function () {
+    highlight();
     document.getElementById(postString).scrollIntoView();
     if (completion) {
       completion();
@@ -88,13 +96,13 @@ var initiallyRequestPostDescriptionWithCompletion = function(postString, complet
   });
 };
 
-var setupAutoScroll = function() {
+function setupAutoScroll() {
   $("#scrollToTop").click( function() {
     $('html, body').animate({scrollTop: '0px'}, 300);
   });
-};
+}
 
-var checkURL = function() {
+function checkURL() {
   if (queryString.localeCompare("post3") == 0) {
     document.getElementById("post3").scrollIntoView();
   }
@@ -106,13 +114,13 @@ var checkURL = function() {
   if (queryString.localeCompare("post1") == 0) {
     document.getElementById("post1").scrollIntoView();
   }
-};
+}
 
 function setup() {
   $( function() { 
     // $("#myNavbar").load("navbar.html");
-    $("#developers").load("developers.html", setupDevs);
-    $("#footer").load("footer.html", setupAutoScroll);
+    $("#developers").load("developers.html", setupDevs());
+    $("#footer").load("footer.html", setupAutoScroll());
     setupCarousel();
     initiallyRequestPostDescriptionWithCompletion("post1", function() {
       initiallyRequestPostDescriptionWithCompletion("post2", function() {
